9d4f12069ff8c6888a0e9bcc82aa473103185b2f
Samad Ghanbari <ghanbari.samad@gmail.com>
2025-09-16 00:22:04 +0330

    tile trying

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { Pool } = require("pg");
 const tilebelt = require("@mapbox/tilebelt");
-const geojsonvt = require("geojson-vt").default || require("geojson-vt");
+const rawGeojsonvt = require("geojson-vt");
+const geojsonvt = rawGeojsonvt.default || rawGeojsonvt;
 const vtpbf = require("vt-pbf");
 const cors = require("cors"); // ← Import cors
 const path = require("path");
@@ -10,7 +11,7 @@ const app = express();
 app.use(cors()); // ← Enable CORS for all origins
 const pool = new Pool({
   user: "admin",
-  host: "10.10.10.96",
+  host: "10.10.10.56",
   database: "gis",
   password: "admin",
   port: 5432,
@@ -21,31 +22,21 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/tiles/:z/:x/:y.pbf", async (req, res) => {
-  const { z, x, y } = req.params;
+  const { z, x, y } = req.params; // z x y are string unary operator + like +z converts it to a number
   const bbox = tilebelt.tileToBBOX([+x, +y, +z]);
+  //console.log(bbox); epsg4326
 
   // BBOX = [minX, minY, maxX, maxY]
   //WHERE way && ST_MakeEnvelope($1, $2, $3, $4, 3857)
-  // const sql = `
-  //   SELECT public.ST_AsGeoJSON(way) AS geometry, name
-  //   FROM planet_osm_line
-  //   WHERE way && ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857)
-  //   LIMIT 1000
-  // `;
-
   const sql = `
-  SELECT ST_AsGeoJSON(way) AS geometry, name
-FROM planet_osm_line
-LIMIT 10;
+    SELECT public.ST_AsGeoJSON(way) AS geometry, name
+    FROM planet_osm_line
+    WHERE way && ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857)
+    LIMIT 1000
   `;
 
-  // console.log("Query BBOX:", bbox);
-  //console.log("BbOx: ", bbox[0], bbox[1], bbox[2], bbox[3]);
-
   try {
-    const result = await pool.query(sql, []);
-
-    //console.log("Rows returned:", result.rows.length);
+    const result = await pool.query(sql, bbox);
 
     const features = result.rows.map((row) => ({
       type: "Feature",
@@ -53,7 +44,10 @@ LIMIT 10;
       properties: { name: row.name },
     }));
 
+    console.log(features[0]["geometry"]["coordinates"]);
+
     const tileIndex = geojsonvt({ type: "FeatureCollection", features });
+
     const tile = tileIndex.getTile(+z, +x, +y);
 
     if (!tile) return res.status(204).send(); // empty tile
@@ -62,6 +56,7 @@ LIMIT 10;
 
     res.setHeader("Content-Type", "application/x-protobuf");
     res.setHeader("Content-Encoding", "gzip"); // optional
+
     res.send(buff);
   } catch (err) {
     console.error("Tile generation error:", err.stack || err.message || err);
