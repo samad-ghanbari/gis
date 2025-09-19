2a1cad8e019ec0245f67fe2d875d93e04f2db0dc
Samad Ghanbari <ghanbari.samad@gmail.com>
2025-09-19 23:38:39 +0330

    draw multi layers of map

--- index.js
+++ index.js
@@ -33,43 +33,93 @@ app.get("/tiles/:z/:x/:y.pbf", async (req, res) => {
 
   // BBOX = [minX, minY, maxX, maxY]
   // query result is 3857
-  /*
-   SELECT public.ST_AsGeoJSON(way) AS geometry, name
+
+  //   const sql = `
+  //   SELECT public.ST_AsGeoJSON(ST_Transform(way, 4326)) AS geometry, name
+  //   FROM planet_osm_line
+  //   WHERE way && ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857)
+  //   LIMIT 1000
+  // `;
+
+  //console.log(z);
+  const limit = z > 12 ? 20000 : z > 10 ? 50000 : 10000;
+
+  const layers = {
+    osm_lines: `
+    SELECT ST_AsGeoJSON(ST_Transform(way, 4326)) AS geometry, name
     FROM planet_osm_line
     WHERE way && ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857)
-    LIMIT 10
-*/
-  // way && bbox is a bounding box intersection operator — it quickly filters way geometries that intersect with the given bounding box.
-
-  const sql = `
-  SELECT public.ST_AsGeoJSON(ST_Transform(way, 4326)) AS geometry, name
-  FROM planet_osm_line
-  WHERE way && ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857)
-  LIMIT 1000
-`;
+    AND highway IS NOT NULL
+    LIMIT ${limit};
+  `,
+    osm_buildings: `
+    SELECT ST_AsGeoJSON(ST_Transform(way, 4326)) AS geometry, name
+    FROM planet_osm_polygon
+    WHERE way && ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857)
+    AND building IS NOT NULL
+    LIMIT ${limit};
+  `,
+    osm_water: `
+    SELECT ST_AsGeoJSON(ST_Transform(way, 4326)) AS geometry, waterway
+    FROM planet_osm_line
+    WHERE way && ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857)
+    AND waterway IS NOT NULL
+    LIMIT ${limit};
+  `,
+  };
 
   try {
-    const values = bbox;
-    const result = await pool.query(sql, values); // , bbox
+    //const values = bbox;
+    // const result = await pool.query(sql, values); // , bbox
 
-    //geojson
-    //features is an array of objects [{}, {}, {}]
-    const features = result.rows.map((row) => ({
-      type: "Feature",
-      geometry: JSON.parse(row.geometry),
-      properties: { name: row.name },
-    }));
+    // //geojson
+    // //features is an array of objects [{}, {}, {}]
+    // const features = result.rows.map((row) => ({
+    //   type: "Feature",
+    //   geometry: JSON.parse(row.geometry),
+    //   properties: { name: row.name },
+    // }));
 
-    // converts large GeoJSON data into vector map tiles on the fly
-    const tileIndex = geojsonvt({ type: "FeatureCollection", features });
-    const tile = tileIndex.getTile(+z, +x, +y);
+    // // converts large GeoJSON data into vector map tiles on the fly
+    // const tileIndex = geojsonvt({ type: "FeatureCollection", features });
+    // const tile = tileIndex.getTile(+z, +x, +y);
 
-    if (!tile) return res.status(204).send();
+    // if (!tile) return res.status(204).send();
 
-    const buff = vtpbf.fromGeojsonVt({ osm: tile });
-    //console.log(buff);
+    // const buff = vtpbf.fromGeojsonVt({ osm: tile });
+    // //console.log(buff);
 
+    // res.setHeader("Content-Type", "application/x-protobuf");
+    // res.send(buff);
+
+    const tileLayers = {};
+
+    for (const [layerName, sql] of Object.entries(layers)) {
+      const result = await pool.query(sql, bbox);
+      const features = result.rows.map((row) => {
+        const { geometry, ...props } = row;
+        return {
+          type: "Feature",
+          geometry: JSON.parse(geometry),
+          properties: props,
+        };
+      });
+      const tileIndex = geojsonvt({ type: "FeatureCollection", features });
+      const tile = tileIndex.getTile(+z, +x, +y);
+      //console.log("zoom level: ", z);
+      if (tile) {
+        tileLayers[layerName] = tile;
+      }
+    }
+
+    const buff = vtpbf.fromGeojsonVt(tileLayers);
     res.setHeader("Content-Type", "application/x-protobuf");
+    // res.setHeader("Content-Encoding", "gzip");
+    // res.setHeader(
+    //   "Content-Disposition",
+    //   `inline; filename="${z}-${x}-${y}.pbf"`
+    // );
+
     res.send(buff);
   } catch (err) {
     console.error("Tile generation error:", err.stack || err.message || err);
